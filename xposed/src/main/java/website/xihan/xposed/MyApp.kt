6e8a20b3b1f64e1e4ffcd6ad8b499e3bd43c7e24
xihan123 <srxqzxs@vip.qq.com>
2025-11-06 17:40:24 +0800

    feat(xposed): 集成KV同步管理器并设置目标包

--- xposed/src/main/java/website/xihan/xposed/MyApp.kt
+++ xposed/src/main/java/website/xihan/xposed/MyApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import website.xihan.kv.KVSyncManager
 
 class MyApp : Application() {
     override fun onCreate() {
@@ -12,5 +13,6 @@ class MyApp : Application() {
             androidContext(this@MyApp)
             androidLogger()
         }
+        KVSyncManager.setTargetPackages("website.xihan.kv.storage")
     }
 }
